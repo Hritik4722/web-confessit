8a189a72e313a22c7312727a39cb4859819d88a8
Yadav Hritik Dinesh <96020100+Hritik4722@users.noreply.github.com>
2025-05-25 23:55:05 +0530

    Update Connectdb64.php

--- Connectdb64.php
+++ Connectdb64.php
@@ -1,13 +1,6 @@
 <?php
 
-// $hostName = "sql304.infinityfree.com";
-// $dbUser = "if0_38711720";
-// $dbPassword = "hritikdine4722";
-// $dbName = "if0_38711720_confess472244";
-// $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
-// if (!$conn) {
-//     die("Something went wrong");
-// }
+
 
 $hostName = "localhost";
 $dbUser = "root";
